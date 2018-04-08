67d4ec7ddda0c154db5a69e56c2c225852b4b879
hoiian <hoiian96@gmail.com>
2018-04-09 02:27:23 +0800

    donegit commit -m ~~

--- hw1/Copy_2_of_F34035031_hw1.m
+++ hw1/Copy_2_of_F34035031_hw1.m
@@ -6,6 +6,7 @@ title('Waveform');
 xlabel('time(s)');
 %fs=44100
 
+
 %----------common value init----------%
 frame_size=20/1000;
 frame_shift=10/1000;
@@ -17,6 +18,7 @@ sample_shift = frame_shift*fs;
 
 sum1=0;
 energy=0;
+% w=window(window_type,window_length);
 w=rectwin(window_length); %rectangle window function
 jj=1;
 
@@ -33,8 +35,12 @@ for i=1:(floor((length(y))/sample_shift)-ceil(window_length/sample_shift))
     jj=1;
 end
 w=0;
+%c=energy;
+%a=((i-1)*sample_shift)+window_length;
 
+% tt=1/fs:(frame_shift/1000):(length(energy)*(frame_shift/1000)) ;
 tt=(1/fs:(length(energy)))/100;
+% ttt=floor((length(y))/sample_shift)-ceil(window_length/sample_shift);
 subplot(5,1,2);
 plot(tt,energy);
 title('Energy');
@@ -42,6 +48,7 @@ xlabel('time(s)');
 
 %------zero crossing rate--------%
 sum2=0;
+% energy=0;
 w=rectwin(window_length); %rectangle window function
 jj=1;
 
@@ -71,7 +78,10 @@ xlabel('time(s)');
           
 %--------------------pitch---------%
 sum4=0;
+% energy=0;
 autocorrelation=0;
+% [y,fs]=audioread(filename);
+% y=y/max(abs(y));
 pitch_freq=0;
 
 
@@ -112,11 +122,13 @@ xlabel('time(s)');
 frameSize = 882;
 overlap = 441;
 y=y-mean(y);				% zero-mean substraction
+%frameMat=buffer2(y, frameSize, overlap);	% frame blocking
+%frameNum=size(frameMat, 2);			% no. of frames
 frameNum = floor((length(y)-1)/window_length)+1;
 
 volume=energy;	
-volumeTh1=max(volume)*0.1;			% volume threshold 1
-% volumeTh1=20;
+% volumeTh1=max(volume)*0.1;			% volume threshold 1
+volumeTh1=20;
 volumeTh2=median(volume)*0.1;			% volume threshold 2
 volumeTh3=min(volume)*10;			% volume threshold 3
 volumeTh4=volume(1)*5;				% volume threshold 4
@@ -132,6 +144,11 @@ endPoint2=([index2(1),index2(end)]-1)*(frameSize-overlap)+round(frameSize/2);
 endPoint3=([index3(1),index3(end)]-1)*(frameSize-overlap)+round(frameSize/2);
 endPoint4=([index4(1),index4(end)]-1)*(frameSize-overlap)+round(frameSize/2);
 
+% endPoint1=frame2sampleIndex([index1(1), index1(end)], frameSize, overlap);
+% endPoint2=frame2sampleIndex([index2(1), index2(end)], frameSize, overlap);
+% endPoint3=frame2sampleIndex([index3(1), index3(end)], frameSize, overlap);
+% endPoint4=frame2sampleIndex([index4(1), index4(end)], frameSize, overlap);
+
 subplot(5,1,4);
 time=(1:length(y))/fs;
 plot(time, y);
@@ -146,3 +163,4 @@ line(time(endPoint4(  1))*[1 1], [-1, 1], 'color', 'r');
 % line(time(endPoint2(end))*[1 1], [-1, 1], 'color', 'g');
 % line(time(endPoint3(end))*[1 1], [-1, 1], 'color', 'k');
 line(time(endPoint4(end))*[1 1], [-1, 1], 'color', 'r');
+% legend('Waveform', 'Boundaries by threshold 1', 'Boundaries by threshold 2', 'Boundaries by threshold 3', 'Boundaries by threshold 4');
